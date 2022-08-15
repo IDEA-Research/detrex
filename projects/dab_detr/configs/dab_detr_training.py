6235eed76bc346a8fc429303ff4b6a8436368227
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-08-15 16:58:06 +0800

    refine training cfg

--- projects/dab_detr/configs/dab_detr_training.py
+++ projects/dab_detr/configs/dab_detr_training.py
@@ -18,3 +18,5 @@ train.max_iter = 375000
 optimizer.weight_decay = 1e-4
 optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in module_name else 1
 
+# modify dataloader config
+dataloader.train.num_workers = 16
