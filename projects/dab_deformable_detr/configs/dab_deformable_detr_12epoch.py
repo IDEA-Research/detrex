817e50d36f48e000f0f78807f2b7621c49c4756a
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-08-16 10:40:41 +0800

    add dab-deformable-detr project

---
+++ projects/dab_deformable_detr/configs/dab_deformable_detr_12epoch.py
@@ -0,0 +1,11 @@
+from ideadet.config import get_config
+
+from .models.dab_detr_r50 import model
+from .common.coco_loader import dataloader
+
+lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_1x
+optimizer = get_config("common/optim.py").AdamW
+train = get_config("common/train.py").train
+
+train.init_checkpoint = "/comp_robot/rentianhe/code/IDEADet/test_new_dab.pth"
+train.output_dir = "./output"
