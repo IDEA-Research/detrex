30ca391b268811ad0f9676bf2bba7e5ab9fc92a1
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-08-19 01:32:04 +0800

    refine training cfgs

--- projects/dab_deformable_detr/configs/dab_deformable_detr_12epoch.py
+++ projects/dab_deformable_detr/configs/dab_deformable_detr_12epoch.py
@@ -2,12 +2,17 @@ from ideadet.config import get_config
 
 from .models.dab_detr_r50 import model
 from .common.coco_loader import dataloader
-from .common.schedule import lr_multiplier_12x
+from .common.schedule import lr_multiplier_12x as lr_multiplier
 
 optimizer = get_config("common/optim.py").AdamW
 train = get_config("common/train.py").train
 
-# train.init_checkpoint = "/comp_robot/rentianhe/code/IDEADet/converted_deformable_dab.pth"
-train.output_dir = "./output/r50_base_12ep"
+train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
+train.output_dir = "./output/r50_deformable_dab_12epoch"
 
-dataloader.train.total_batch_size = 16
+
+optimizer.lr = 1e-4
+optimizer.weight_decay = 1e-4
+optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in module_name else 1
+
+dataloader.train.num_workers = 16
