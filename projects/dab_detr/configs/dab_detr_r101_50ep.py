df2182bbdedd3d0f6440490ecf2678e2c6a124ad
Ren Tianhe <48727989+rentainhe@users.noreply.github.com>
2022-09-01 00:51:23 +0800

    Combine amp and simple trainer together (#38)
    * combine amp and simple trainer
    
    * refine config
    
    * add train.sh
    
    Co-authored-by: ntianhe ren <rentianhe@dgx061.scc.idea>

--- projects/dab_detr/configs/dab_detr_r101_50ep.py
+++ projects/dab_detr/configs/dab_detr_r101_50ep.py
@@ -1,7 +1,7 @@
 from .dab_detr_r50_50ep import model, dataloader, lr_multiplier, optimizer, train
 
 # modify training config
-train.init_checkpoint = "./pretrained_weights/r101.pkl"
+train.init_checkpoint = "path/to/R-101.pkl"
 train.output_dir = "./output/dab_detr_r101_50ep"
 
 # modify model config
