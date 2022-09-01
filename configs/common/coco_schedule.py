de6a91f0bb50a86c92a88f698904c921f8718fc4
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-09-01 13:23:58 +0800

    remove redundant configs in dab-detr

--- configs/common/coco_schedule.py
+++ configs/common/coco_schedule.py
@@ -38,8 +38,45 @@ def default_X_scheduler(num_X):
     )
 
 
+def default_coco_scheduler(epochs=50, decay_epochs=40, warmup_epochs=0):
+    """
+    Returns the config for a default multi-step LR scheduler such as "1x", "3x",
+    commonly referred to in papers, where every 1x has the total length of 1440k
+    training images (~12 COCO epochs). LR is decayed twice at the end of training
+    following the strategy defined in "Rethinking ImageNet Pretraining", Sec 4.
+    
+    Args:
+        epochs (int): total training epochs.
+        decay_epochs (int): lr decay steps.
+        warmup_epochs (int): warmup epochs.
+    
+    Returns:
+        DictConfig: configs that define the multiplier for LR during training
+    """
+    # total number of iterations assuming 16 batch size, using 1440000/16=90000
+    total_steps_16bs = epochs * 7500
+    decay_steps = decay_epochs * 7500
+    warmup_steps = warmup_epochs * 7500
+    scheduler = L(MultiStepParamScheduler)(
+        values=[1.0, 0.1],
+        milestones=[decay_steps, total_steps_16bs],
+    )
+    return L(WarmupParamScheduler)(
+        scheduler=scheduler,
+        warmup_length=warmup_steps / total_steps_16bs,
+        warmup_method="linear",
+        warmup_factor=0.001,
+    )
+
+
+# default coco scheduler
 lr_multiplier_1x = default_X_scheduler(1)
 lr_multiplier_2x = default_X_scheduler(2)
 lr_multiplier_3x = default_X_scheduler(3)
 lr_multiplier_6x = default_X_scheduler(6)
 lr_multiplier_9x = default_X_scheduler(9)
+
+
+# default scheduler for detr
+lr_multiplier_50ep = default_coco_scheduler(50, 40, 0)
+lr_multiplier_12ep = default_coco_scheduler(12, 11, 0)
