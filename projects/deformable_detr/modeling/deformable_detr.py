31e1b72fcdeed991791dcead5df591c9cc02e997
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-09-05 01:07:51 +0800

    add deformable-detr project

---
+++ projects/deformable_detr/modeling/deformable_detr.py
@@ -0,0 +1,271 @@
+# coding=utf-8
+# Copyright 2022 The IDEA Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import copy
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from detrex.layers import MLP, box_cxcywh_to_xyxy, box_xyxy_to_cxcywh
+from detrex.utils import inverse_sigmoid
+
+from detectron2.modeling import detector_postprocess
+from detectron2.structures import Boxes, ImageList, Instances
+
+
+class DabDeformableDETR(nn.Module):
+    def __init__(
+        self,
+        backbone,
+        position_embedding,
+        neck,
+        transformer,
+        num_classes,
+        num_queries,
+        criterion,
+        pixel_mean,
+        pixel_std,
+        embed_dim=256,
+        aux_loss=True,
+        with_box_refine=False,
+        as_two_stage=False,
+        device="cuda",
+    ):
+        super().__init__()
+        self.backbone = backbone
+        self.neck = neck
+        self.position_embedding = position_embedding
+        self.transformer = transformer
+        self.num_queries = num_queries
+        self.class_embed = nn.Linear(embed_dim, num_classes)
+        self.bbox_embed = MLP(embed_dim, embed_dim, 4, 3)
+        self.num_classes = num_classes
+
+        if not as_two_stage:
+            self.query_embedding = nn.Embedding(num_queries, embed_dim * 2)
+
+        self.aux_loss = aux_loss
+        self.with_box_refine = with_box_refine
+        self.as_two_stage = as_two_stage
+        self.criterion = criterion
+
+        # normalizer for input raw images
+        self.device = device
+        pixel_mean = torch.Tensor(pixel_mean).to(self.device).view(3, 1, 1)
+        pixel_std = torch.Tensor(pixel_std).to(self.device).view(3, 1, 1)
+        self.normalizer = lambda x: (x - pixel_mean) / pixel_std
+                
+        # if two-stage, the last class_embed and bbox_embed is for region proposal generation
+        num_pred = (
+            (transformer.decoder.num_layers + 1) if as_two_stage else transformer.decoder.num_layers
+        )
+        if with_box_refine:
+            self.class_embed = nn.ModuleList([copy.deepcopy(self.class_embed) for i in range(num_pred)])
+            self.bbox_embed = nn.ModuleList([copy.deepcopy(self.bbox_embed) for i in range(num_pred)])
+            nn.init.constant_(self.bbox_embed[0].layers[-1].bias.data[2:], -2.0)
+            self.transformer.decoder.bbox_embed = self.bbox_embed
+        else:
+            nn.init.constant_(self.bbox_embed.layers[-1].bias.data[2:], -2.0)
+            self.class_embed = nn.ModuleList([self.class_embed for _ in range(num_pred)])
+            self.bbox_embed = nn.ModuleList([self.bbox_embed for _ in range(num_pred)])
+            self.transformer.decoder.bbox_embed = None
+
+        if self.as_two_stage:
+            # hack implementation for two-stage
+            self.transformer.decoder.class_embed = self.class_embed
+            for box_embed in self.bbox_embed:
+                nn.init.constant_(box_embed.layers[-1].bias.data[2:], 0.0)
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        prior_prob = 0.01
+        bias_value = -math.log((1 - prior_prob) / prior_prob)
+
+        for class_embed_layer in self.class_embed:
+            class_embed_layer.bias.data = torch.ones(self.num_classes) * bias_value
+
+        for bbox_embed_layer in self.bbox_embed:
+            nn.init.constant_(bbox_embed_layer.layers[-1].weight.data, 0)
+            nn.init.constant_(bbox_embed_layer.layers[-1].bias.data, 0)
+
+        for _, neck_layer in self.neck.named_modules():
+            if isinstance(neck_layer, nn.Conv2d):
+                nn.init.xavier_uniform_(neck_layer.weight, gain=1)
+                nn.init.constant_(neck_layer.bias, 0)
+
+
+    def forward(self, batched_inputs):
+
+        images = self.preprocess_image(batched_inputs)
+
+        if self.training:
+            batch_size, _, H, W = images.tensor.shape
+            img_masks = images.tensor.new_ones(batch_size, H, W)
+            for img_id in range(batch_size):
+                img_h, img_w = batched_inputs[img_id]["instances"].image_size
+                img_masks[img_id, :img_h, :img_w] = 0
+        else:
+            batch_size, _, H, W = images.tensor.shape
+            img_masks = images.tensor.new_zeros(batch_size, H, W)
+
+        # original features
+        features = self.backbone(images.tensor)  # output feature dict
+
+        multi_level_feats = self.neck(features)
+        multi_level_masks = []
+        multi_level_position_embeddings = []
+
+        for feat in multi_level_feats:
+            multi_level_masks.append(
+                F.interpolate(img_masks[None], size=feat.shape[-2:]).to(torch.bool).squeeze(0)
+            )
+            multi_level_position_embeddings.append(self.position_embedding(multi_level_masks[-1]))
+
+        query_embeds = None
+        if not self.as_two_stage:
+            query_embeds = self.query_embedding.weight
+
+        (
+            inter_states,
+            init_reference,
+            inter_references,
+            enc_outputs_class,
+            enc_outputs_coord_unact,
+        ) = self.transformer(
+            multi_level_feats, multi_level_masks, multi_level_position_embeddings, query_embeds
+        )
+
+        outputs_classes = []
+        outputs_coords = []
+        for lvl in range(inter_states.shape[0]):
+            if lvl == 0:
+                reference = init_reference
+            else:
+                reference = inter_references[lvl - 1]
+            reference = inverse_sigmoid(reference)
+            outputs_class = self.class_embed[lvl](inter_states[lvl])
+            tmp = self.bbox_embed[lvl](inter_states[lvl])
+            if reference.shape[-1] == 4:
+                tmp += reference
+            else:
+                assert reference.shape[-1] == 2
+                tmp[..., :2] += reference
+            outputs_coord = tmp.sigmoid()
+            outputs_classes.append(outputs_class)
+            outputs_coords.append(outputs_coord)
+        outputs_class = torch.stack(outputs_classes)
+        outputs_coord = torch.stack(outputs_coords)
+
+        output = {"pred_logits": outputs_class[-1], "pred_boxes": outputs_coord[-1]}
+        if self.aux_loss:
+            output["aux_outputs"] = self._set_aux_loss(outputs_class, outputs_coord)
+
+        if self.as_two_stage:
+            enc_outputs_coord = enc_outputs_coord_unact.sigmoid()
+            output["enc_outputs"] = {'pred_logits': enc_outputs_class, 'pred_boxes': enc_outputs_coord}
+
+        if self.training:
+            gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
+            targets = self.prepare_targets(gt_instances)
+            loss_dict = self.criterion(output, targets)
+            weight_dict = self.criterion.weight_dict
+            for k in loss_dict.keys():
+                if k in weight_dict:
+                    loss_dict[k] *= weight_dict[k]
+            return loss_dict
+        else:
+            box_cls = output["pred_logits"]
+            box_pred = output["pred_boxes"]
+            results = self.inference(box_cls, box_pred, images.image_sizes)
+            processed_results = []
+            for results_per_image, input_per_image, image_size in zip(
+                results, batched_inputs, images.image_sizes
+            ):
+                height = input_per_image.get("height", image_size[0])
+                width = input_per_image.get("width", image_size[1])
+                r = detector_postprocess(results_per_image, height, width)
+                processed_results.append({"instances": r})
+            return processed_results
+
+    def preprocess_image(self, batched_inputs):
+        images = [self.normalizer(x["image"].to(self.device)) for x in batched_inputs]
+        images = ImageList.from_tensors(images)
+        return images
+
+    def inference(self, box_cls, box_pred, image_sizes):
+        """
+        Arguments:
+            box_cls (Tensor): tensor of shape (batch_size, num_queries, K).
+                The tensor predicts the classification probability for each query.
+            box_pred (Tensor): tensors of shape (batch_size, num_queries, 4).
+                The tensor predicts 4-vector (x,y,w,h) box
+                regression values for every queryx
+            image_sizes (List[torch.Size]): the input image sizes
+
+        Returns:
+            results (List[Instances]): a list of #images elements.
+        """
+        assert len(box_cls) == len(image_sizes)
+        results = []
+
+        # box_cls.shape: 1, 300, 80
+        # box_pred.shape: 1, 300, 4
+        prob = box_cls.sigmoid()
+        topk_values, topk_indexes = torch.topk(prob.view(box_cls.shape[0], -1), 100, dim=1)
+        scores = topk_values
+        topk_boxes = torch.div(topk_indexes, box_cls.shape[2], rounding_mode="floor")
+        labels = topk_indexes % box_cls.shape[2]
+
+        boxes = torch.gather(box_pred, 1, topk_boxes.unsqueeze(-1).repeat(1, 1, 4))
+
+        # For each box we assign the best class or the second best if the best on is `no_object`.
+        # scores, labels = F.softmax(box_cls, dim=-1)[:, :, :-1].max(-1)
+
+        for i, (scores_per_image, labels_per_image, box_pred_per_image, image_size) in enumerate(
+            zip(scores, labels, boxes, image_sizes)
+        ):
+            result = Instances(image_size)
+            result.pred_boxes = Boxes(box_cxcywh_to_xyxy(box_pred_per_image))
+
+            result.pred_boxes.scale(scale_x=image_size[1], scale_y=image_size[0])
+            result.scores = scores_per_image
+            result.pred_classes = labels_per_image
+            results.append(result)
+        return results
+
+    def prepare_targets(self, targets):
+        new_targets = []
+        for targets_per_image in targets:
+            h, w = targets_per_image.image_size
+            image_size_xyxy = torch.as_tensor([w, h, w, h], dtype=torch.float, device=self.device)
+            gt_classes = targets_per_image.gt_classes
+            gt_boxes = targets_per_image.gt_boxes.tensor / image_size_xyxy
+            gt_boxes = box_xyxy_to_cxcywh(gt_boxes)
+            new_targets.append({"labels": gt_classes, "boxes": gt_boxes})
+        return new_targets
+
+    @torch.jit.unused
+    def _set_aux_loss(self, outputs_class, outputs_coord):
+        # this is a workaround to make torchscript happy, as torchscript
+        # doesn't support dictionary with non-homogeneous values, such
+        # as a dict having both a Tensor and a list.
+        return [
+            {"pred_logits": a, "pred_boxes": b}
+            for a, b in zip(outputs_class[:-1], outputs_coord[:-1])
+        ]
