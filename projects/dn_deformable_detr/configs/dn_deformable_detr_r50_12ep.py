ed1efc089cf696f9e40d84a57003e056fe07220d
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-09-05 23:26:02 +0800

    add dn_deformable_detr 12ep config

--- projects/dn_deformable_detr/configs/dn_deformable_detr_r50_12ep.py
+++ projects/dn_deformable_detr/configs/dn_deformable_detr_r50_12ep.py
@@ -3,13 +3,13 @@ from .models.dn_deformable_detr_r50 import model
 
 dataloader = get_config("common/data/coco_detr.py").dataloader
 optimizer = get_config("common/optim.py").AdamW
-lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_50ep
+lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_12ep
 train = get_config("common/train.py").train
 
 # modify training config
 train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
-train.output_dir = "./output/dn_deformable_detr_r50_50ep"
-train.max_iter = 375000
+train.output_dir = "./output/dn_deformable_detr_r50_12ep"
+train.max_iter = 90000
 train.clip_grad.enabled = True
 train.clip_grad.params.max_norm = 0.1
 train.clip_grad.params.norm_type = 2
