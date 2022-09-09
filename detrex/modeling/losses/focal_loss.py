be2811623faf74ebe00af1b8991155f681909dba
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-09-09 12:19:07 +0800

    add focal loss module

--- detrex/modeling/losses/focal_loss.py
+++ detrex/modeling/losses/focal_loss.py
@@ -17,38 +17,94 @@
 # ------------------------------------------------------------------------------------------------
 # Modified from:
 # https://github.com/facebookresearch/detr/blob/main/models/segmentation.py
+# https://github.com/facebookresearch/fvcore/blob/main/fvcore/nn/focal_loss.py
 # ------------------------------------------------------------------------------------------------
 
-
+import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
+from .utils import reduce_loss
 
-def sigmoid_focal_loss(inputs, targets, num_boxes, alpha: float = 0.25, gamma: float = 2):
+def sigmoid_focal_loss(
+    preds, 
+    targets, 
+    alpha: float = 0.25, 
+    gamma: float = 2,
+    reduction: str = "mean",
+    avg_factor: int = None,
+):
     """
     Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
 
     Args:
-        inputs (torch.Tensor): A float tensor of arbitrary shape.
+        preds (torch.Tensor): A float tensor of arbitrary shape.
             The predictions for each example.
         targets (torch.Tensor): A float tensor with the same shape as inputs. Stores the binary
             classification label for each element in inputs
             (0 for the negative class and 1 for the positive class).
-        num_boxes (int): The number of boxes.
         alpha (float, optional): Weighting factor in range (0, 1) to balance
             positive vs negative examples. Default: 0.25.
         gamma (float): Exponent of the modulating factor (1 - p_t) to
             balance easy vs hard examples. Default: 2.
+        reduction: 'none' | 'mean' | 'sum'
+                 'none': No reduction will be applied to the output.
+                 'mean': The output will be averaged.
+                 'sum': The output will be summed.
+        avg_factor (int): Average factor that is used to average 
+            the loss. Default: None.
 
     Returns:
-        torch.Tensor: The computed sigmoid focal loss.
+        torch.Tensor: The computed sigmoid focal loss with the reduction option applied.
     """
-    prob = inputs.sigmoid()
-    ce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
-    p_t = prob * targets + (1 - prob) * (1 - targets)
+    preds = preds.float()
+    targets = targets.float()
+    p = torch.sigmoid(preds)
+    ce_loss = F.binary_cross_entropy_with_logits(preds, targets, reduction="none")
+    p_t = p * targets + (1 - p) * (1 - targets)
     loss = ce_loss * ((1 - p_t) ** gamma)
 
     if alpha >= 0:
         alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
         loss = alpha_t * loss
 
-    return loss.mean(1).sum() / num_boxes
+    if avg_factor is None:
+        loss = reduce_loss(loss, reduction=reduction)
+    else:
+        if reduction == "mean":
+            eps = torch.finfo(torch.float32).eps
+            loss = loss.sum() / (avg_factor + eps)
+        elif reduction != 'none':
+            raise ValueError('avg_factor can not be used with reduction="sum"')
+
+    return loss
+
+
+class FocalLoss(nn.Module):
+    def __init__(
+        self,
+        alpha=0.25,
+        gamma=2.0,
+        reduction="mean",
+        loss_weight=1.0,
+    ):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction=reduction
+        self.loss_weight = loss_weight
+    
+    def forward(
+        self,
+        preds,
+        targets,
+        avg_factor=None,
+    ):
+        loss_class = self.loss_weight * sigmoid_focal_loss(
+            preds,
+            targets,
+            self.alpha,
+            self.gamma,
+            self.reduction,
+            avg_factor,
+        )
+        return loss_class
