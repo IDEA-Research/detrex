739cb692393f4dea82ca7bac9489c9387f667a42
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-09-17 16:47:09 +0800

    refine detr args

--- projects/detr/configs/models/detr_r50.py
+++ projects/detr/configs/models/detr_r50.py
@@ -1,13 +1,6 @@
-import torch.nn as nn
-
-from detectron2.modeling.backbone import ResNet, BasicStem
 from detectron2.config import LazyCall as L
 
-from detrex.layers import (
-    BaseTransformerLayer,
-    FFN,
-    MultiheadAttention,
-)
+from detrex.modeling.backbone import ResNet, BasicStem
 from detrex.modeling.matcher import HungarianMatcher
 from detrex.modeling.criterion.criterion import SetCriterion
 from detrex.layers.position_embedding import PositionEmbeddingSine
@@ -30,55 +23,37 @@ model = L(DETR)(
         out_features=["res2", "res3", "res4", "res5"],
         freeze_at=1,
     ),
-    position_embedding=L(PositionEmbeddingSine)(num_pos_feats=128, normalize=True),
     in_features=["res5"],
+    in_channels=2048,
+    position_embedding=L(PositionEmbeddingSine)(
+        num_pos_feats=128, 
+        temperature=10000,
+        normalize=True
+    ),
     transformer=L(DetrTransformer)(
         encoder=L(DetrTransformerEncoder)(
-            transformer_layers=L(BaseTransformerLayer)(
-                attn=L(MultiheadAttention)(
-                    embed_dim=256,
-                    num_heads=8,
-                    attn_drop=0.1,
-                    batch_first=False,
-                ),
-                ffn=L(FFN)(
-                    embed_dim=256,
-                    feedforward_dim=2048,
-                    ffn_drop=0.1,
-                ),
-                norm=L(nn.LayerNorm)(normalized_shape=256),
-                operation_order=("self_attn", "norm", "ffn", "norm"),
-            ),
+            embed_dim=256,
+            num_heads=8,
+            attn_dropout=0.1,
+            feedforward_dim=2048,
+            ffn_dropout=0.1,
             num_layers=6,
             post_norm=False,
         ),
         decoder=L(DetrTransformerDecoder)(
+            embed_dim=256,
+            num_heads=8,
+            attn_dropout=0.1,
+            feedforward_dim=2048,
+            ffn_dropout=0.1,
             num_layers=6,
             return_intermediate=True,
-            transformer_layers=L(BaseTransformerLayer)(
-                attn=L(MultiheadAttention)(
-                    embed_dim=256,
-                    num_heads=8,
-                    attn_drop=0.1,
-                    batch_first=False,
-                ),
-                ffn=L(FFN)(
-                    embed_dim=256,
-                    feedforward_dim=2048,
-                    ffn_drop=0.1,
-                ),
-                norm=L(nn.LayerNorm)(
-                    normalized_shape=256,
-                ),
-                operation_order=("self_attn", "norm", "cross_attn", "norm", "ffn", "norm"),
-            ),
             post_norm=True,
         ),
     ),
-    num_classes=80,  # 80 categories and 1 for non-object
-    num_queries=100,
     embed_dim=256,
-    in_channels=2048,
+    num_classes=80,
+    num_queries=100,
     criterion=L(SetCriterion)(
         num_classes=80,
         matcher=L(HungarianMatcher)(
@@ -92,11 +67,8 @@ model = L(DETR)(
             "loss_bbox": 5.0,
             "loss_giou": 2.0,
         },
+        loss_class_type="ce_loss",
         eos_coef=0.1,
-        losses=[
-            "class",
-            "boxes",
-        ],
     ),
     aux_loss=True,
     pixel_mean=[123.675, 116.280, 103.530],
