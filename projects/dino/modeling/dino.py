00ad3585869a766dcf6365d393e645dea3f9ad50
SlongLiu <slongliu86@gmail.com>
2022-09-19 17:17:31 +0800

    update dino

--- projects/dino/modeling/dino.py
+++ projects/dino/modeling/dino.py
@@ -16,6 +16,7 @@
 
 import copy
 import math
+from typing import List
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -28,42 +29,79 @@ from detectron2.structures import Boxes, ImageList, Instances
 
 
 class DINO (nn.Module):
+    """Implement DAB-Deformable-DETR in `DAB-DETR: Dynamic Anchor Boxes are Better Queries for DETR 
+    <https://arxiv.org/abs/2203.03605>`_.
+
+    Code is modified from the `official github repo
+    <https://github.com/IDEA-Research/DINO>`_.
+
+    Args:
+        backbone (nn.Module): backbone module
+        position_embedding (nn.Module): position embedding module
+        neck (nn.Module): neck module
+        transformer (nn.Module): transformer module
+        embed_dim (int): dimension of embedding
+        num_classes (int): Number of total categories.
+        num_queries (int): Number of proposal dynamic anchor boxes in Transformer
+        criterion (nn.Module): Criterion for calculating the total losses.
+        pixel_mean (List[float]): Pixel mean value for image normalization. 
+            Default: [123.675, 116.280, 103.530].
+        pixel_std (List[float]): Pixel std value for image normalization.
+            Default: [58.395, 57.120, 57.375].
+        aux_loss (bool): Whether to calculate auxiliary loss in criterion. Default: True.
+        select_box_nums_for_evaluation (int): the number of topk candidates 
+            slected at postprocess for evaluation. Default: 100.
+        device (str): Training device. Default: "cuda".
+    """
+
     def __init__(
         self,
-        backbone,
-        position_embedding,
-        neck,
-        transformer,
-        num_classes,
-        num_queries,
-        criterion,
-        pixel_mean,
-        pixel_std,
-        embed_dim=256,
-        aux_loss=True,
+        backbone: nn.Module,
+        position_embedding: nn.Module,
+        neck: nn.Module,
+        transformer: nn.Module,
+        embed_dim: int,
+        num_classes: int,
+        num_queries: int,
+        criterion: nn.Module,
+        pixel_mean: List[float] = [123.675, 116.280, 103.530],
+        pixel_std: List[float] = [123.675, 116.280, 103.530],
+        aux_loss: bool = True,
+        select_box_nums_for_evaluation: int = 300,
         device="cuda",
-        dn_number=100,
-        label_noise_ratio=0.2,
-        box_noise_scale=1.0,
-
+        dn_number: int = 100,
+        label_noise_ratio: float = 0.2,
+        box_noise_scale: float = 1.0,
     ):
         super().__init__()
+        # define backbone and position embedding module
         self.backbone = backbone
-        self.neck = neck
         self.position_embedding = position_embedding
-        self.transformer = transformer
+
+        # define neck module
+        self.neck = neck
+
+        # number of dynamic anchor boxes and embedding dimension
         self.num_queries = num_queries
+        self.embed_dim = embed_dim
+
+        # define transformer module
+        self.transformer = transformer
+
+        # define classification head and box head
         self.class_embed = nn.Linear(embed_dim, num_classes)
         self.bbox_embed = MLP(embed_dim, embed_dim, 4, 3)
         self.num_classes = num_classes
-        self.embed_dim=embed_dim
+
+        # where to calculate auxiliary loss in criterion
+        self.aux_loss = aux_loss
+        self.criterion = criterion
+
+        # denoising
         self.label_enc=nn.Embedding(num_classes,embed_dim)
         self.dn_number=dn_number
         self.label_noise_ratio=label_noise_ratio
         self.box_noise_scale=box_noise_scale
-
-        self.aux_loss = aux_loss
-        self.criterion = criterion
 
         # normalizer for input raw images
         self.device = device
@@ -92,12 +130,36 @@ class DINO (nn.Module):
         self.transformer.decoder.class_embed = self.class_embed
         self.transformer.decoder.bbox_embed = self.bbox_embed
 
+        # hack implementation for two-stage
         for bbox_embed_layer in self.bbox_embed:
             nn.init.constant_(bbox_embed_layer.layers[-1].bias.data[2:], 0.0)
 
+        # set topk boxes selected for inference
+        self.select_box_nums_for_evaluation = select_box_nums_for_evaluation
 
     def forward(self, batched_inputs):
+        """Forward function of `DAB-DETR` which excepts a list of dict as inputs.
 
+        Args:
+            batched_inputs (List[dict]): A list of instance dict, and each instance dict must consists of:
+                - dict["image"] (torch.Tensor): The unnormalized image tensor.
+                - dict["height"] (int): The original image height.
+                - dict["width"] (int): The original image width.
+                - dict["instance"] (detectron2.structures.Instances): Image meta informations and ground truth boxes and labels during training.
+                    Please refer to https://detectron2.readthedocs.io/en/latest/modules/structures.html#detectron2.structures.Instances
+                    for the basic usage of Instances.
+        
+        Returns:
+            dict: Returns a dict with the following elements:
+                - dict["pred_logits"]: the classification logits for all queries (anchor boxes in DAB-DETR).
+                            with shape ``[batch_size, num_queries, num_classes]``
+                - dict["pred_boxes"]: The normalized boxes coordinates for all queries in format
+                    ``(x, y, w, h)``. These values are normalized in [0, 1] relative to the size of 
+                    each individual image (disregarding possible padding). See PostProcess for information 
+                    on how to retrieve the unnormalized bounding box.
+                - dict["aux_outputs"]: Optional, only returned when auxilary losses are activated. It is a list of
+                            dictionnaries containing the two above keys for each decoder layer.
+        """
         images = self.preprocess_image(batched_inputs)
 
         if self.training:
@@ -113,15 +175,19 @@ class DINO (nn.Module):
         # original features
         features = self.backbone(images.tensor)  # output feature dict
 
+        # project backbone features to the reuired dimension of transformer
+        # we use multi-scale features in DINO
         multi_level_feats = self.neck(features)
         multi_level_masks = []
         multi_level_position_embeddings = []
-
         for feat in multi_level_feats:
             multi_level_masks.append(
                 F.interpolate(img_masks[None], size=feat.shape[-2:]).to(torch.bool).squeeze(0)
             )
             multi_level_position_embeddings.append(self.position_embedding(multi_level_masks[-1]))
+
+        # denoising preprocessing
+        # prepare label query embedding
         if self.training:
             gt_instances = [x["instances"].to(self.device) for x in batched_inputs]
             targets = self.prepare_targets(gt_instances)
@@ -130,10 +196,10 @@ class DINO (nn.Module):
                                  box_noise_scale=self.box_noise_scale, num_queries=self.num_queries,
                                  num_classes=self.num_classes, hidden_dim=self.embed_dim, label_enc=self.label_enc)
         else:
-            input_query_label, input_query_bbox, attn_mask, dn_meta=None,None,None,None
-
+            input_query_label, input_query_bbox, attn_mask, dn_meta = None, None, None, None
         query_embeds = (input_query_label, input_query_bbox)
 
+        # feed into transformer
         (
             inter_states,
             init_reference,
@@ -143,7 +209,10 @@ class DINO (nn.Module):
         ) = self.transformer(
             multi_level_feats, multi_level_masks, multi_level_position_embeddings, query_embeds,attn_masks=[attn_mask,None]
         )
+        # hack implementation for distributed training
         inter_states[0] += self.label_enc.weight[0, 0] * 0.0
+
+        # Calculate output coordinates and classes.
         outputs_classes = []
         outputs_coords = []
         for lvl in range(inter_states.shape[0]):
@@ -163,10 +232,16 @@ class DINO (nn.Module):
             outputs_classes.append(outputs_class)
             outputs_coords.append(outputs_coord)
         outputs_class = torch.stack(outputs_classes)
+            # tensor shape: [num_decoder_layers, bs, num_query, num_classes]
         outputs_coord = torch.stack(outputs_coords)
+            # tensor shape: [num_decoder_layers, bs, num_query, 4]
+
+        # denoising postprocessing
         if dn_meta is not None:
             outputs_class, outputs_coord = \
                 self.dn_post_process(outputs_class, outputs_coord,dn_meta)
+
+        # prepare for loss computation
         output = {"pred_logits": outputs_class[-1], "pred_boxes": outputs_coord[-1]}
         if self.aux_loss:
             output["aux_outputs"] = self._set_aux_loss(outputs_class, outputs_coord)
@@ -175,9 +250,9 @@ class DINO (nn.Module):
         interm_coord = enc_reference
         interm_class = self.transformer.decoder.class_embed[-1](enc_state)
         output['enc_outputs'] = {'pred_logits': interm_class, 'pred_boxes': interm_coord}
-        # output['dn_meta']=dn_meta
-        if self.training:
 
+
+        if self.training:
             loss_dict = self.criterion(output, targets, dn_meta)
             weight_dict = self.criterion.weight_dict
             for k in loss_dict.keys():
@@ -349,7 +424,7 @@ class DINO (nn.Module):
         # box_cls.shape: 1, 300, 80
         # box_pred.shape: 1, 300, 4
         prob = box_cls.sigmoid()
-        topk_values, topk_indexes = torch.topk(prob.view(box_cls.shape[0], -1), 300, dim=1)
+        topk_values, topk_indexes = torch.topk(prob.view(box_cls.shape[0], -1), self.select_box_nums_for_evaluation, dim=1)
         scores = topk_values
         topk_boxes = torch.div(topk_indexes, box_cls.shape[2], rounding_mode="floor")
         labels = topk_indexes % box_cls.shape[2]
