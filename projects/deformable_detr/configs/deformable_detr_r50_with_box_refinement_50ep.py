6218186db0f719fbe06a11d47099c37cb9733253
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-09-26 15:46:09 +0800

    refine output_dir

--- projects/deformable_detr/configs/deformable_detr_r50_with_box_refinement_50ep.py
+++ projects/deformable_detr/configs/deformable_detr_r50_with_box_refinement_50ep.py
@@ -11,4 +11,4 @@ model.with_box_refine = True
 
 # modify training config
 train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
-train.output_dir = "./output/deformable_detr_box_refine_50ep"
+train.output_dir = "./output/deformable_detr_with_box_refinement_50ep"
