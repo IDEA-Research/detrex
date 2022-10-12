3eb64444d931fea35a24321ed508cd3e5f90ac66
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-10-12 12:53:02 +0800

    add 24ep config

---
+++ projects/dino/configs/dino_r50_4scale_24ep.py
@@ -0,0 +1,32 @@
+from detrex.config import get_config
+from .models.dino_r50 import model
+
+# get default config
+dataloader = get_config("common/data/coco_detr.py").dataloader
+optimizer = get_config("common/optim.py").AdamW
+lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_24ep
+train = get_config("common/train.py").train
+
+# modify model config
+model.position_embedding.temperature = 20
+model.position_embedding.offset = 0.
+
+# modify training config
+train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
+train.output_dir = "./output/retrain_dino_r50_4scale_24ep_20drop"
+train.max_iter = 180000
+train.clip_grad.enabled = True
+train.clip_grad.params.max_norm = 0.1
+train.clip_grad.params.norm_type = 2
+train.seed = 42
+
+# modify optimizer config
+optimizer.weight_decay = 1e-4
+optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in module_name else 1
+
+# modify dataloader config
+dataloader.train.dataset.filter_empty = False
+dataloader.train.num_workers = 16
+
+# dump the testing results into output_dir for visualization
+dataloader.evaluator.output_dir = train.output_dir
