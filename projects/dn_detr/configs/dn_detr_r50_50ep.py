5e343630bd46a5671dc80346d05e2525768ed9b6
Ren Tianhe <48727989+rentainhe@users.noreply.github.com>
2022-10-18 23:23:06 +0800

    [Enhance] build more clear configs for detrex projects (#107)
    * refine README
    
    * build more clear config files for detrex models
    
    Co-authored-by: ntianhe ren <rentianhe@dgx061.scc.idea>

--- projects/dn_detr/configs/dn_detr_r50_50ep.py
+++ projects/dn_detr/configs/dn_detr_r50_50ep.py
@@ -9,15 +9,41 @@ train = get_config("common/train.py").train
 # modify training config
 train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
 train.output_dir = "./output/dn_detr_r50_50ep"
+
+# max training iterations
 train.max_iter = 375000
+
+# run evaluation every 5000 iters
+train.eval_period = 5000
+
+# log training infomation every 20 iters
+train.log_period = 20
+
+# save checkpoint every 5000 iters
+train.checkpointer.period = 5000
+
+# gradient clipping for training
 train.clip_grad.enabled = True
 train.clip_grad.params.max_norm = 0.1
 train.clip_grad.params.norm_type = 2
-train.seed = 42
+
+# set training devices
+train.device = "cuda"
+model.device = train.device
 
 # modify optimizer config
+optimizer.lr = 1e-4
+optimizer.betas = (0.9, 0.999)
 optimizer.weight_decay = 1e-4
 optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in module_name else 1
 
 # modify dataloader config
 dataloader.train.num_workers = 16
+
+# please notice that this is total batch size.
+# surpose you're using 4 gpus for training and the batch size for
+# each gpu is 16/4 = 4
+dataloader.train.total_batch_size = 16
+
+# dump the testing results into output_dir for visualization
+dataloader.evaluator.output_dir = train.output_dir
