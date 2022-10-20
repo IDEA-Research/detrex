52980108c6b5e455188d2380da914dc144fe2f34
jiading <jiading.biz@outlook.com>
2022-10-20 11:11:44 +0800

    support H-Deformable-DETR (#110)

---
+++ projects/h_deformable_detr/configs/h_deformable_detr_r50_50ep.py
@@ -0,0 +1,25 @@
+from detrex.config import get_config
+from .models.h_deformable_detr_r50 import model
+
+dataloader = get_config("common/data/coco_detr.py").dataloader
+lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_50ep
+optimizer = get_config("common/optim.py").AdamW
+train = get_config("common/train.py").train
+
+# modify training config
+train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
+train.output_dir = "./output/h_deformable_detr_r50_50ep"
+train.max_iter = 375000
+train.clip_grad.enabled = True
+train.clip_grad.params.max_norm = 0.1
+train.clip_grad.params.norm_type = 2
+train.seed = 42
+
+# modify optimizer config
+optimizer.weight_decay = 1e-4
+optimizer.params.lr_factor_func = (
+    lambda module_name: 0.1 if "backbone" in module_name else 1
+)
+
+# modify dataloader config
+dataloader.train.num_workers = 16
---
+++ projects/h_deformable_detr/configs/h_deformable_detr_swin_large_two_stage_36ep.py
@@ -0,0 +1,32 @@
+from .h_deformable_detr_r50_50ep import train, dataloader, optimizer, model
+from detrex.config import get_config
+from detectron2.config import LazyCall as L
+from detectron2.layers import ShapeSpec
+from detectron2.modeling.backbone import SwinTransformer
+
+lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_36ep
+# modify model config
+model.backbone = L(SwinTransformer)(
+    pretrain_img_size=224,
+    embed_dim=192,
+    depths=(2, 2, 18, 2),
+    num_heads=(6, 12, 24, 48),
+    drop_path_rate=0.5,
+    window_size=7,
+    out_indices=(1, 2, 3),
+)
+
+# modify neck config
+model.neck.input_shapes = {
+    "p1": ShapeSpec(channels=384),
+    "p2": ShapeSpec(channels=768),
+    "p3": ShapeSpec(channels=1536),
+}
+model.neck.in_features = ["p1", "p2", "p3"]
+model.with_box_refine = True
+model.as_two_stage = True
+
+# modify training config
+train.init_checkpoint = "/mnt/pretrained_backbone/swin_large_patch4_window7_224_22k.pth"
+train.output_dir = "./output/h_deformable_detr_swin_large_two_stage_36ep"
+train.max_iter = 270000
