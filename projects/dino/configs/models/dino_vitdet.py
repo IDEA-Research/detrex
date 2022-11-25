4757809cfac137caba18df52a914d48a0221c590
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-11-25 14:36:38 +0800

    refine ViTDet DINO config

--- projects/dino/configs/models/dino_vitdet.py
+++ projects/dino/configs/models/dino_vitdet.py
@@ -41,7 +41,7 @@ model.backbone = L(SimpleFeaturePyramid)(
     ),
     in_feature="${.net.out_feature}",
     out_channels=256,
-    scale_factors=(4.0, 2.0, 1.0, 0.5),
+    scale_factors=(2.0, 1.0, 0.5),  # (4.0, 2.0, 1.0, 0.5) in ViTDet
     top_block=L(LastLevelMaxPool)(),
     norm="LN",
     square_pad=1024,
@@ -49,12 +49,11 @@ model.backbone = L(SimpleFeaturePyramid)(
 
 # modify neck config
 model.neck.input_shapes = {
-    "p2": ShapeSpec(channels=256),
     "p3": ShapeSpec(channels=256),
     "p4": ShapeSpec(channels=256),
     "p5": ShapeSpec(channels=256),
     "p6": ShapeSpec(channels=256),
 }
-model.neck.in_features = ["p2", "p3", "p4", "p5", "p6"]
-model.neck.num_outs = 5
-model.transformer.num_feature_levels = 5
+model.neck.in_features = ["p3", "p4", "p5", "p6"]
+model.neck.num_outs = 4
+model.transformer.num_feature_levels = 4
