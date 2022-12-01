47bc8595e39c7c04f4a0ba2b3379de4879044532
ntianhe ren <rentianhe@dgx061.scc.idea>
2022-12-01 10:06:07 +0800

    add warmup scheduler for vitdet-large

--- projects/dino/configs/dino_vitdet_large_4scale_50ep.py
+++ projects/dino/configs/dino_vitdet_large_4scale_50ep.py
@@ -1,4 +1,6 @@
-from detrex.config import get_config
+from fvcore.common.param_scheduler import MultiStepParamScheduler
+from detectron2.config import LazyCall as L
+from detectron2.solver import WarmupParamScheduler
 
 from .dino_vitdet_large_4scale_12ep import (
     train,
@@ -7,10 +9,18 @@ from .dino_vitdet_large_4scale_12ep import (
     model,
 )
 
-# modify lr-multiplier config
-lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_50ep
 
 # modify training config
 train.max_iter = 375000
 train.init_checkpoint = "detectron2://ImageNetPretrained/MAE/mae_pretrain_vit_large.pth"
 train.output_dir = "./output/dino_vitdet_large_50ep"
+
+# warmup lr scheduler
+lr_multiplier = L(WarmupParamScheduler)(
+    scheduler=L(MultiStepParamScheduler)(
+        values=[1.0, 0.1],
+        milestones=[300000, 375000],
+    ),
+    warmup_length=250 / train.max_iter,
+    warmup_factor=0.001,
+)
