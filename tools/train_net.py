eef5ac32922617d8fdd918eeb341df55ebc654fe
Ren Tianhe <48727989+rentainhe@users.noreply.github.com>
2023-02-03 10:51:02 +0800

    [Features] Support fast-dev-run for fast debugging (#197)
    * add fast-dev-run
    
    * enable fast-dev-run config
    
    * refine docs
    
    ---------
    
    Co-authored-by: ntianhe ren <rentianhe@dgx061.scc.idea>

--- tools/train_net.py
+++ tools/train_net.py
@@ -42,8 +42,6 @@ from detrex.utils import WandbWriter
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
-logger = logging.getLogger("detrex")
-
 
 class Trainer(SimpleTrainer):
     """
@@ -244,6 +242,12 @@ def main(args):
     cfg = LazyConfig.load(args.config_file)
     cfg = LazyConfig.apply_overrides(cfg, args.opts)
     default_setup(cfg, args)
+    
+    # Enable fast debugging by running several iterations to check for any bugs.
+    if cfg.train.fast_dev_run.enabled:
+        cfg.train.max_iter = 20
+        cfg.train.eval_period = 10
+        cfg.train.log_period = 1
 
     if args.eval_only:
         model = instantiate(cfg.model)
