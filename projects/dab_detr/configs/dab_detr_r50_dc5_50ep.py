42bed94e05a652e705fe9949e61a976336315ec6
rentainhe <596106517@qq.com>
2023-02-14 17:47:49 +0800

    refine dab-detr-r50-dc5 model with torchvision resnet

--- projects/dab_detr/configs/dab_detr_r50_dc5_50ep.py
+++ projects/dab_detr/configs/dab_detr_r50_dc5_50ep.py
@@ -7,5 +7,5 @@ from .dab_detr_r50_50ep import (
 from .models.dab_detr_r50_dc5 import model
 
 # modify training config
-train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
+train.init_checkpoint = "https://download.pytorch.org/models/resnet50-0676ba61.pth"
 train.output_dir = "./output/dab_detr_r50_dc5_50ep"
--- projects/dab_detr/configs/models/dab_detr_r50_dc5.py
+++ projects/dab_detr/configs/models/dab_detr_r50_dc5.py
@@ -1,17 +1,12 @@
 from detectron2.config import LazyCall as L
-from detrex.modeling.backbone import ResNet, BasicStem, make_stage
+from detrex.modeling.backbone.torchvision_resnet import TorchvisionResNet
 
 from .dab_detr_r50 import model
 
 
-model.backbone = L(ResNet)(
-    stem=L(BasicStem)(in_channels=3, out_channels=64, norm="FrozenBN"),
-    stages=L(make_stage)(
-        depth=50,
-        stride_in_1x1=False,
-        norm="FrozenBN",
-        res5_dilation=2,
-    ),
-    out_features=["res2", "res3", "res4", "res5"],
-    freeze_at=1,
+model.backbone=L(TorchvisionResNet)(
+    name="resnet50",
+    train_backbone=True,
+    dilation=True,
+    return_layers={"layer4": "res5"}
 )
