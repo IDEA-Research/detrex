722f9d4dbfe633467226d9b64fa291ed87e8cebe
rentainhe <596106517@qq.com>
2023-03-19 02:40:57 +0800

    add group-detr 12ep config

--- projects/group_detr/configs/group_detr_r50_12ep.py
+++ projects/group_detr/configs/group_detr_r50_12ep.py
@@ -3,15 +3,15 @@ from .models.group_detr_r50 import model
 
 dataloader = get_config("common/data/coco_detr.py").dataloader
 optimizer = get_config("common/optim.py").AdamW
-lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_50ep
+lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_12ep
 train = get_config("common/train.py").train
 
 # modify training config
 train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
-train.output_dir = "./output/group_detr_r50_50ep"
+train.output_dir = "./output/group_detr_r50_12ep"
 
 # max training iterations
-train.max_iter = 375000
+train.max_iter = 90000
 train.eval_period = 7500
 train.checkpointer.period = 7500
 
@@ -29,9 +29,6 @@ optimizer.lr = 1e-4
 optimizer.betas = (0.9, 0.999)
 optimizer.weight_decay = 1e-4
 optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in module_name else 1
-
-# modify dataloader config
-dataloader.train.num_workers = 16
 
 # please notice that this is total batch size.
 # surpose you're using 4 gpus for training and the batch size for
