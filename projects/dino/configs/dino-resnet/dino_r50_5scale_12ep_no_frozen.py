8654d93a70dd9ddec4fba993753503ab9a1d76cf
rentainhe <596106517@qq.com>
2023-04-27 10:45:30 +0800

    refine

--- projects/dino/configs/dino-resnet/dino_r50_5scale_12ep_no_frozen.py
+++ projects/dino/configs/dino-resnet/dino_r50_5scale_12ep_no_frozen.py
@@ -6,6 +6,7 @@ from .dino_r50_5scale_12ep import (
     model,
 )
 
+# no frozen backbone for better result
 model.backbone.freeze_at = -1
 
 train.output_dir = "./output/dino_r50_5scale_12ep_no_frozen_backbone"
